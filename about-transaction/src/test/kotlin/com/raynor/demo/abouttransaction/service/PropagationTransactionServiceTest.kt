2831930ee5dbe691bbe87b48dedf4fd787201fd8
Raynor <jonggu.woo@fitpet.co.kr>
2024-09-10 17:53:57 +0900

    흠좀무...

--- about-transaction/src/test/kotlin/com/raynor/demo/abouttransaction/service/PropagationTransactionServiceTest.kt
+++ about-transaction/src/test/kotlin/com/raynor/demo/abouttransaction/service/PropagationTransactionServiceTest.kt
@@ -1,29 +1,21 @@
 package com.raynor.demo.abouttransaction.service
 
+import com.raynor.demo.abouttransaction.repository.CategoryRepository
 import com.raynor.demo.abouttransaction.repository.ProductRepository
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class AboutTransactionServiceTest(
+class PropagationTransactionServiceTest(
     private val productRepository: ProductRepository,
-    private val basicTransactionService: BasicTransactionService,
+    private val categoryRepository: CategoryRepository,
     private val propagationTransactionService: PropagationTransactionService
 ) : FunSpec({
 
     beforeEach {
-        productRepository.deleteAllInBatch()
-    }
-
-    test("t - 1") {
-        /*
-SET autocommit=0
-insert into `product` (`created_at`,`deleted`,`name`,`price`,`stock_quantity`) values ('2024-08-02 07:47:03.609793',0,'상품 1 5b742715-bdbb-4a08-920c-eb79bbfdf23e',20,56)
-commit
-SET autocommit=1
-        * */
-        basicTransactionService.insertWithBasicTransaction()
+        productRepository.deleteAll()
+        categoryRepository.deleteAll()
     }
 
     test("t - 2") {
@@ -52,8 +44,6 @@ insert into `product` (`created_at`,`deleted`,`name`,`price`,`stock_quantity`) v
 commit
 SET autocommit=1
         * */
-        shouldThrow<Exception> {
-            propagationTransactionService.insertWithNewTransactionAndSubMethodException()
-        }
+        propagationTransactionService.insertWithNewTransaction()
     }
 })
