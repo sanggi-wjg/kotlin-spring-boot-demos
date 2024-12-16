cda37eec9c3985a899139a77d104386cab91379d
Raynor <jonggu.woo@fitpet.co.kr>
2024-12-16 16:19:14 +0900

    ✨ about-core 작성 중...

---
+++ about-core/README.md
@@ -0,0 +1,7 @@
+# Spring Framework
+
+docs 폴더 참조
+
+## Ref
+
+* https://docs.spring.io/spring-framework/reference/core/beans/introduction.html
--- about-core/src/main/kotlin/com/raynor/demo/aboutcore/Application.kt
+++ about-core/src/main/kotlin/com/raynor/demo/aboutcore/Application.kt
@@ -1,11 +1,17 @@
 package com.raynor.demo.aboutcore
 
+import com.raynor.demo.aboutcore.service.ProductService
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 @SpringBootApplication
 class Application
 
 fun main(args: Array<String>) {
     runApplication<Application>(*args)
+
+    val context = AnnotationConfigApplicationContext(ProductService::class.java)
+    val service = context.getBean("productService")
+    println(service)
 }
