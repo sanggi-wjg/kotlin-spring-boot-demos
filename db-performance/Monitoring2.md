c80c28d68a048fe0bf83b2d648c42b2504922c26
Raynor <jonggu.woo@fitpet.co.kr>
2025-01-01 00:28:42 +0900

    ✨ DB vendor performance, 잉잉앙앙 울어라

---
+++ db-performance/Monitoring2.md
@@ -0,0 +1,11 @@
+# MySQL 2core 4gb 로 설정해서 테스트 해봄
+
+## 바닐라
+
+![](images/.Monitoring2_images/6cb0acf2.png)
+
+## 풀사이즈 20, innodb 관련 설정 조금
+
+![](images/.Monitoring2_images/845ff1f4.png)
+
+![](images/.Monitoring2_images/48cd2d6d.png)
--- db-performance/benchmark_tool/main.py
+++ db-performance/benchmark_tool/main.py
@@ -3,9 +3,9 @@ from locust import task, between, TaskSet, HttpUser, FastHttpUser
 
 class BenchmarkTask(TaskSet):
 
-    # @task
-    # def mysql_create(self):
-    #     self.client.get("/independent/mysql-create")
+    @task
+    def mysql_create(self):
+        self.client.get("/independent/mysql-create")
 
     @task
     def mysql_read(self):
