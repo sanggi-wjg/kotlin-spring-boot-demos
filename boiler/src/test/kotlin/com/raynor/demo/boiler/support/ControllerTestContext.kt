cf737efdfd0f3652b6847396396d5d18f2e20bd4
Raynor <jonggu.woo@fitpet.co.kr>
2026-09-11 11:20:53 +0900

    Add OrderControllerTest 작성 및 ControllerTestContext 업데이트

--- boiler/src/test/kotlin/com/raynor/demo/boiler/support/ControllerTestContext.kt
+++ boiler/src/test/kotlin/com/raynor/demo/boiler/support/ControllerTestContext.kt
@@ -1,7 +1,9 @@
 package com.raynor.demo.boiler.support
 
 import com.ninjasquad.springmockk.MockkBean
+import com.raynor.demo.boiler.controller.order.OrderController
 import com.raynor.demo.boiler.controller.product.ProductController
+import com.raynor.demo.boiler.service.order.OrderService
 import com.raynor.demo.boiler.service.product.ProductService
 import io.kotest.core.extensions.ApplyExtension
 import io.kotest.core.spec.style.FunSpec
@@ -14,6 +16,7 @@ import org.springframework.context.annotation.Import
 @WebMvcTest(
     value = [
         ProductController::class,
+        OrderController::class,
     ],
 )
 @Import()
@@ -36,4 +39,7 @@ abstract class ControllerTestContext(
     ) {
     @MockkBean
     private lateinit var productService: ProductService
+
+    @MockkBean
+    private lateinit var orderService: OrderService
 }
